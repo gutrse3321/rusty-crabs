ab0ea292a4f1a7955321f2438b2b09fdc46f17c5
gutrse3321 <gutrse3321@live.com>
2020-06-03 15:20:14 +0800

    集合

---
+++ collections/src/main.rs
@@ -0,0 +1,60 @@
+use std::fmt::Pointer;
+
+fn main() {
+    let v = vec![1, 2, 3];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = Vec::new();
+    v.push(5);
+    v.push(6);
+    v.push(7);
+
+    let third: &i32 = &v[2];
+    println!("&v[2]: {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("match third ele: {}", third),
+        None => println!("no match"),
+    }
+
+    for i in &mut v {
+        *i += 50;
+        println!("{}", i);
+    }
+
+    // 使用枚举存多种类型
+    let row = vec![
+        SpreadSheetCell::Int(3),
+        SpreadSheetCell::Text("yellow".to_string()),
+        SpreadSheetCell::Float(10.10),
+    ];
+    let int = &row[0];
+    match int {
+        SpreadSheetCell::Int(i) => {
+            println!("int: {}", i);
+        },
+        SpreadSheetCell::Float(_) => {},
+        SpreadSheetCell::Text(_) => {},
+    }
+}
+
+enum SpreadSheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
+
+use std::collections::HashMap;
+
+fn func_hash_map() {
+    let mut scores = HashMap::new();
+    scores.insert("blue".to_string(), 10);
+    scores.insert("yellow".to_string(), 50);
+
+    // 使用队伍列表和分数列表创建
+    let teams = vec!["blue".to_string(), "yellow".to_string()];
+    let ini_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+}
